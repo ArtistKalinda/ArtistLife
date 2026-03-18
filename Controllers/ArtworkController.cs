d4f76d0bc89cf4bcf4e0db1aec749ae56dcdecf6
Kalinda Ghavit <kalindaghavit@gmail.com>
2026-03-18 18:19:43 +0530

    Initial commit - ArtistLife project

---
+++ Controllers/ArtworkController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ArtistLife.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ArtworkController : ControllerBase
+    {
+
+        [HttpGet]
+        public IActionResult GetArtworks()
+        {
+            return Ok("All artworks");
+        }
+
+        [HttpPost]
+        public IActionResult CreateArtwork()
+        {
+            return Ok("Artwork created");
+        }
+
+    }
+}
---
+++ Model/Artwork.cs
@@ -0,0 +1,17 @@
+﻿namespace ArtistLife.API.Model
+{
+    public class Artwork
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public int UserId { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+    }
+}
